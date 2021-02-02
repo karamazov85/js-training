9d3102840202fc2b309253cf7bb5da192107d799
karamazov85 <farkas.adam.mail@gmail.com>
2021-02-02 14:41:04 +0000

    added merge arrays exerise

---
+++ algo/merge-arrays/merge-arrays.js
@@ -0,0 +1,24 @@
+// Given two or more arrays, write a function that combines their elements into one array without any repetition.
+
+const mergeArrays = (...args) => { // we don't know how many args we will have. So we spread them in
+    // note you can work with ...arrays like array.something()
+
+    // check if function received correct arguments
+    for (let arg of args) {
+        if(!Array.isArray(arg)){
+            throw new TypeError("one or more arguments not an array")
+        }
+    }
+
+    // flatten:
+    const flat = args.reduce((acc,curr) => { return acc.concat(curr) },[])
+
+    // For practive's sake, the 3 different ways to get rid of duplicates from an array
+    const uniqueValues = [ ...new Set(flat) ]
+    const uniqueValues2 = flat.reduce((acc, curr) => { return acc.includes(curr) ? acc : [ ...acc, curr ]}, [])
+    const uniqueValues3 = flat.filter((value, index) => flat.indexOf(value) === index)
+    
+    return uniqueValues;
+}
+
+module.exports = mergeArrays;
---
+++ algo/merge-arrays/merge-arrays.test.js
@@ -0,0 +1,11 @@
+const mergeArrays = require('./merge-arrays');
+
+test(' merges arrays without duplicates', () => {
+    expect(mergeArrays([1,2,3,4,5],[5,6,7,8,9],[9,9],[11,13,45,8])).toEqual([1,2,3,4,5,6,7,8,9,11,13,45])
+})
+
+test(' throws Error if one of the arguments is not an array', () => {
+    // note: if you want to check if a a function throws the right error, 
+    // you have to wrap the function in an anonymous function inside expect()!!!
+    expect(() => {mergeArrays([1,2,4], [4,5,6], "kite")}).toThrow(TypeError);
+})
